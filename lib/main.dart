80709f6ea59f2603f2599ad3997673e19c8dfeeb
devabacus <devabacusnrf@gmail.com>
2021-02-08 09:03:49 +0500

    add packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final firestoreInstance = FirebaseFirestore.instance;
-  final firebaseAuth = FirebaseAuth.instance;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(appBar: AppBar(title: Text("Task points"),)),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("task points"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: (){
+              print("hello man, you just pressed me");
+            },
+            child: Text("Button", style: TextStyle(fontSize: 30),),
+          ),
+        ),
+      ),
     );
   }
-
-  @override
-  void initState() {
-    firebaseAuth.signInAnonymously().then((value) => print("value.user.uid = ${value.user.uid}"));
-    firestoreInstance.collection("users").add({
-      "name": "Катя",
-      "age": 33,
-      "email": "ekaterita20@gmail.com",
-      "address": {"street": "Ismagilova", "city": "ufa"}
-    }).then((value) => print(value));
-  }
-  }
+}
